bd533e0beaf150f9ddaddf6491844365e8cbf9e1
pstha369 <109883995+pstha369@users.noreply.github.com>
2022-09-27 16:49:45 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
+/*Global scope*/
+"use strict";
 var hours,minutes,seconds,ampm,time;
 var hrs = document.querySelector(".hrs");
 var mns = document.querySelector(".mns");
 var scs = document.querySelector(".scs");
 var x = setInterval(function(){
-        const date = new Date();
-        hours=date.getHours();
-        minutes=date.getMinutes();
-        seconds=date.getSeconds();
+        const date = new Date();/*creating object for Date*/
+        hours = date.getHours();/*getting the current hour*/
+        minutes = date.getMinutes();/*getting the current minute*/
+        seconds = date.getSeconds();/*getting the current second*/
         ampm = hours < 12 ? "AM" : "PM";
-        hours = hours < 12 ? hours : hours - 12;
+        hours = hours < 12 ? hours : hours - 12;/*conversion of 24 hour format into 12 hour format*/
         hours = hours < 10 ? "0" + hours : hours;
         minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-        time=hours+":"+minutes+":"+seconds + " " + ampm;
-        document.getElementById("digital-clock").innerHTML=time;
-        analog(hours,minutes,seconds);
-},100);
+        seconds = seconds < 10 ? "0" + seconds : seconds;/*adding 0 before hour,minutes or seconds, less than 10*/
+        time=hours+":"+minutes+":"+seconds + " " + ampm;/*storing current time in (hours:minutes:second AM/PM) format*/ 
+        document.getElementById("digital-clock").innerHTML=time;/*displaying time*/
+        analog();
+},500);/*calling this function every 500 milliseconds */
 
-function analog(h,m,s){
-    hrs.style.transform=`rotate(calc(30deg * ${h}))`;
-    mns.style.transform=`rotate(calc(6deg * ${m}))`;
-    scs.style.transform=`rotate(calc(6deg * ${s}))`;
+function analog(){
+    hrs.style.transform=`rotate(calc(30deg * ${hours}))`;
+    mns.style.transform=`rotate(calc(6deg * ${minutes}))`;
+    scs.style.transform=`rotate(calc(6deg * ${seconds}))`;
 }
-
